9788cea5d6fc51746be8c4364af6f1a2de358334
Kingpin2557 <lode.snaet@student.howest.be>
2025-07-09 20:11:31 +0200

    feat: init

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,19 @@
-'use strict';
-import '../scss/styles.scss';
+"use strict";
+import { capitalize } from "./test";
+import "../scss/styles.scss";
 
 const init = function () {
-    console.log('hello world!');
+  const msg = capitalize("Hello World!");
+  console.log(msg);
 };
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
-import * as bootstrap from 'bootstrap';
+// import * as bootstrap from 'bootstrap';
+
+// Accept HMR as per: https://vitejs.dev/guide/api-hmr.html
+if (import.meta.hot) {
+  import.meta.hot.accept(() => {
+    console.log("HMR active");
+  });
+}
---
+++ src/js/test.js
@@ -0,0 +1,8 @@
+// src/js/utils/helpers.js
+export function capitalize(str) {
+  return str.toUpperCase();
+}
+
+export function sum(a, b) {
+  return a + b;
+}

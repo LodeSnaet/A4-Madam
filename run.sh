a9b995949a08e9eb256abbe69d249eb1396eb31a
Kingpin2557 <lode.snaet@student.howest.be>
2025-08-01 12:23:22 +0200

    feat: app.js instead of app.ts

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Start PHP-FPM in the background
-php-fpm &
-
-# Start Nginx in the foreground
-exec nginx -g 'daemon off;'
--- vite.config.js
+++ vite.config.js
@@ -6,46 +6,47 @@ import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-    base: command === 'serve' ? '' : `${process.env.CRAFT_CLOUD_ARTIFACT_BASE_URL || ''}/dist/`,
-    build: {
-        emptyOutDir: true,
-        manifest: "manifest.json",
-        outDir: "../cms/web/dist",
-        rollupOptions: {
-            input: {
-                "app": "./src/js/app.ts",
-            },
-        },
-        sourcemap: true,
+  base:
+    command === "serve"
+      ? ""
+      : `${process.env.CRAFT_CLOUD_ARTIFACT_BASE_URL || ""}/dist/`,
+  build: {
+    emptyOutDir: true,
+    manifest: "manifest.json",
+    outDir: "../cms/web/dist",
+    rollupOptions: {
+      input: {
+        app: "./src/js/app.js",
+      },
     },
-    envDir: command === "build" ? "../../cms/web/environment" : "./",
-    plugins: [
-        ViteRestart({
-            reload: ["./src/templates/**/*"],
-        }),
-    ],
-    publicDir: "./src/public",
-    resolve: {
-        alias: [
-            { find: "~", replacement: path.resolve(__dirname, "./src") },
-        ],
-        preserveSymlinks: true,
+    sourcemap: true,
+  },
+  envDir: command === "build" ? "../../cms/web/environment" : "./",
+  plugins: [
+    ViteRestart({
+      reload: ["./src/templates/**/*"],
+    }),
+  ],
+  publicDir: "./src/public",
+  resolve: {
+    alias: [{ find: "~", replacement: path.resolve(__dirname, "./src") }],
+    preserveSymlinks: true,
+  },
+  server: {
+    // Allow cross-origin requests -- https://github.com/vitejs/vite/security/advisories/GHSA-vg6x-rcgg-rjx6
+    allowedHosts: true,
+    cors: {
+      origin: /(\.local|\.site|localhost)/,
     },
-    server: {
-        // Allow cross-origin requests -- https://github.com/vitejs/vite/security/advisories/GHSA-vg6x-rcgg-rjx6
-        allowedHosts: true,
-        cors: {
-            origin: /(\.local|\.site|localhost)/,
-        },
-        fs: {
-            strict: false,
-        },
-        headers: {
-            "Access-Control-Allow-Private-Network": "true",
-        },
-        host: "0.0.0.0",
-        origin: "http://localhost:3000",
-        port: 3000,
-        strictPort: true,
+    fs: {
+      strict: false,
     },
+    headers: {
+      "Access-Control-Allow-Private-Network": "true",
+    },
+    host: "0.0.0.0",
+    origin: "http://localhost:3000",
+    port: 3000,
+    strictPort: true,
+  },
 }));
